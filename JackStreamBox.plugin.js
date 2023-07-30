f0e60bfdb26adde8174ecf0a7ea8ac55c4c736c0
CodeChrisB <c.buchberger01@gmail.com>
2023-07-30 23:13:00 +0200

    More Stable Discord Plugin

--- JackStreamBox.plugin.js
+++ JackStreamBox.plugin.js
@@ -1,4 +1,3 @@
-// JavaScript source code
 /**
  * @name JackStreamBox
  * @author CodeChrisB
@@ -6,33 +5,36 @@
  * @version 0.0.1
  */
 
-function StartStreaming() {
-  let vcName = "Jackbot";
-  //Join VC
-  let listItem = document.querySelector(`li[data-dnd-name="${vcName}"]`);
-  let vcATag = listItem.getElementsByClassName("mainContent-20q_Hp")[0];
-  vcATag.click();
-  //Stream Button
-  let StreamBtn =document.getElementsByClassName("button-12Fmur enabled-9OeuTA button-ejjZWC lookBlank-FgPMy6 colorBrand-2M3O3N grow-2T4nbg button-12Fmur")[0]
-  if(!StreamBtn) return
-  StreamBtn.click()
+async function StartStreaming() {
+    let vcName = "Jackbot";
 
-  setTimeout(()=>{
+    //------Join Server
+    let server = document.querySelector(`div[data-list-item-id="guildsnav___697504479834275898"]`);
+    server.click()
+    //------Join VC
+    await new Promise(resolve => setTimeout(resolve, 500));
+    let listItem = document.querySelector(`li[data-dnd-name="${vcName}"]`); //Get correct vc
+    let vcATag = listItem.querySelector("a"); //get link to join
+    vcATag.click();
+    //------Open the Stream UI
+    await new Promise(resolve => setTimeout(resolve, 100));
+    let StreamBtn = document.getElementsByClassName("button-12Fmur enabled-9OeuTA button-ejjZWC lookBlank-FgPMy6 colorBrand-2M3O3N grow-2T4nbg button-12Fmur")[0]
+    if (!StreamBtn) return
+    StreamBtn.click()
+    //------Wait for Stream UI to pop up then start stream
+    await new Promise(resolve => setTimeout(resolve, 800));
     let goLiveBtn = document.querySelectorAll(`button[type="submit"]`)
-    if(!goLiveBtn) return
+    if (!goLiveBtn) return
     console.log(goLiveBtn)
-    //when the bot bugs it might open multiple widows thus goLiveBtn is an NodeList
     goLiveBtn[0].click()
-    
-  }, 1000);
 }
 
 
 
 
 module.exports = class {
-  start() {
-    StartStreaming()
-  }
-  stop() {}
+    start() {
+        StartStreaming()
+    }
+    stop() { }
 };
